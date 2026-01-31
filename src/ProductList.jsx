14b66287153f43c9977fdf329c4759f14e48339b
Niranjan <niranjantuty@gmail.com>
2026-01-31 03:05:50 -0500

    deployment changes

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
-import addItem from CartSlice;
 import CartSlice from './CartSlice';
+
 function ProductList({ onHomeClick }) {
     const [showCart, setShowCart] = useState(false);
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
